03e9d3cd8f32566c2c75cc6825200d9b094af0cd
xujif <i@xujif.com>
2016-06-15 18:12:47 +0800

    修改

--- src/Eloquent/Traits/PostgresJsonSupprot.php
+++ src/Eloquent/Traits/PostgresJsonSupprot.php
@@ -2,31 +2,12 @@
 namespace Xujif\LaravelPgSqlSchema\Eloquent\Traits;
 
 use Illuminate\Database\Eloquent\Builder;
-use Xujif\LaravelPgSqlSchema\Eloquent\Utils;
 
 trait PostgresJsonSupport
 {
-    protected function _operatorToFun($operator)
-    {
-        $arr = [
-            '?' => 'json_exists($column,?)',
-            '?|'=> 'json_exists_any($column,?)',
-            '?&'=> 'json_exists_all($column,?)',
-        ];
-        return isset($arr[$operator]) ? $arr[$operator] : false;
-    }
-
-
 
     public function scopeWherePgJson(Builder $builder, $column, $operator, $value)
     {
-        if (in_array($operator, ['?|', '?&'])) {
-            $value = Utils::array2pgArray($value);
-        }
-        $fun = $this->_operatorToFun($operator);
-        if ($fun) {
-            return $builder->whereRaw(str_replace('$column', $column, $fun), [$value]);
-        }
         return $builder->whereRaw("$column $operator ?", [$value]);
     }
 }
--- src/Eloquent/Traits/PostgresJsonbSupport.php
+++ src/Eloquent/Traits/PostgresJsonbSupport.php
@@ -1,19 +1,29 @@
 <?php
 namespace Xujif\LaravelPgSqlSchema\Eloquent\Traits;
 
-
 trait PostgresJsonbSupport
 {
-    use PostgresJsonSupport;
 
     protected function _operatorToFun($operator)
     {
         $arr = [
             '?' => 'jsonb_exists($column,?)',
-            '?|'=> 'jsonb_exists_any($column,?)',
-            '?&'=> 'jsonb_exists_all($column,?)',
+            '?|' => 'jsonb_exists_any($column,?)',
+            '?&' => 'jsonb_exists_all($column,?)',
         ];
         return isset($arr[$operator]) ? $arr[$operator] : false;
+    }
+
+    public function scopeWherePgJsonb(Builder $builder, $column, $operator, $value)
+    {
+        if (in_array($operator, ['?|', '?&'])) {
+            $value = Utils::array2pgArray($value);
+        }
+        $fun = $this->_operatorToFun($operator);
+        if ($fun) {
+            return $builder->whereRaw(str_replace('$column', $column, $fun), [$value]);
+        }
+        return $builder->whereRaw("$column $operator ?", [$value]);
     }
 
 }
